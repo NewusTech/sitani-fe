327b3c59b3f97a05bb2601190c48cf89e439b1e2
hariando7 <rianaja2200022@gmail.com>
2024-08-26 23:01:35 +0700

    add-beritaPage

--- src/app/(landing-page)/berita/page.tsx
+++ src/app/(landing-page)/berita/page.tsx
@@ -1,24 +1,66 @@
+import CardBeritaPage from '@/components/landing-page/card-berita-page';
+import { Input } from '@/components/ui/input';
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 import Link from 'next/link';
 import React from 'react'
 
 const BeritaPage = () => {
-    const articles = [
-        { id: 1, title: 'Berita 1', summary: 'Ringkasan berita 1' },
-        { id: 2, title: 'Berita 2', summary: 'Ringkasan berita 2' },
-        // Tambahkan lebih banyak berita sesuai kebutuhan
-    ];
     return (
         <div>
-            <h1>Berita Terbaru</h1>
-            {articles.map((article) => (
-                <div key={article.id} className="card">
-                    <h2>{article.title}</h2>
-                    <p>{article.summary}</p>
-                    <Link href={`/${article.id}`}>
-                        <p>Read More</p>
+            <div className='w-full min-h-screen mt-28'>
+                {/* berita terkini */}
+                <div className="berita container mx-auto py-[60px]">
+                    <div className="header items-center flex gap-5 justify-between">
+                        <div className="div">
+                            <div className="text-primary font-semibold text-3xl flex-shrink-0">Berita</div>
+                            <div className="div">Berita terkait Dinas Ketahanan Pangan, Tanamaan Pangan dan Holtikultura</div>
+                        </div>
+                        <div className="searc">
+                            <Input placeholder="Cari" className='w-[370px] border border-primary' />
+                        </div>
+                    </div>
+                    {/* card */}
+                    <div className="berita mt-[60px] grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <CardBeritaPage />
+                        <CardBeritaPage />
+                        <CardBeritaPage />
+                        <CardBeritaPage />
+                        <CardBeritaPage />
+                        <CardBeritaPage />
+                    </div>
+                    <Link href="" className="selengkapnya">
                     </Link>
+                    {/* pagination */}
+                    <div className="pagination flex justify-end">
+                        <Pagination className='justify-end'>
+                            <PaginationContent>
+                                <PaginationItem>
+                                    <PaginationPrevious href="#" />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationLink href="#">1</PaginationLink>
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationLink href="#" isActive>
+                                        2
+                                    </PaginationLink>
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationLink href="#">3</PaginationLink>
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationEllipsis />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationNext href="#" />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </div>
+                    {/* pagination */}
                 </div>
-            ))}
+                {/* berita terkini */}
+            </div>
         </div>
     )
 }
---
+++ src/components/landing-page/card-berita-page/index.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import React from 'react'
+import ArrowBeritaPage from '../../../../public/icons/ArrowBeritaPage'
+
+interface CardBeritaPageProps {
+    image?: string
+    date?: string
+    title?: string
+    desc?: string
+}
+
+const CardBeritaPage = (props: CardBeritaPageProps) => {
+    return (
+        <>
+            <div className="rounded-xl bg-primary-600 p-4 flex gap-4">
+                <div className="flex items-center justify-center rounded overflow-hidden">
+                    <Image
+                        src="/assets/images/cardBeritaPage.png"
+                        alt="logo"
+                        width={300}
+                        height={300}
+                        unoptimized
+                        className="object-cover"
+                    />
+                </div>
+                <div className="flex flex-col gap-4 w-full">
+                    <div className="mt-4 text-[#2C2C2C] text-[12px]">
+                        January 13, 2024
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <div className="text-[#2C2C2C] text-[20px] font-semibold">
+                            Lorem Ipsum Dolor Amet Amit Amon Amin
+                        </div>
+                        <ArrowBeritaPage />
+                    </div>
+                    <div className="text-sm text-[#2C2C2C]">
+                        Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig. Terabel pov astrobel ?
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CardBeritaPage
