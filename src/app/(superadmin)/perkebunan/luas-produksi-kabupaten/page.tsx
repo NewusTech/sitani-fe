08554329c44883c001367110b00d009c906d8c68
hariando7 <rianaja2200022@gmail.com>
2024-09-02 14:27:10 +0700

    add table perkebunan kabupaten

--- src/app/(superadmin)/perkebunan/luas-produksi-kabupaten/page.tsx
+++ src/app/(superadmin)/perkebunan/luas-produksi-kabupaten/page.tsx
@@ -14,6 +14,16 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 import Link from 'next/link'
 
 import { format } from "date-fns"
@@ -25,9 +35,56 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import EyeIcon from '../../../../../public/icons/EyeIcon'
+import EditIcon from '../../../../../public/icons/EditIcon'
+import DeletePopup from '@/components/superadmin/PopupDelete'
+
+interface Data {
+    kecamatan?: string;
+    desa?: string;
+    hasilProduksi?: string;
+    namaTanaman?: string;
+    luasTanamanAkhirBulanLalu?: string;
+    luasPanen: {
+        habisDibongkar?: number;
+        belumHabis?: number;
+    }
+    luasRusak?: string;
+    luasPenanamanBaru?: string;
+    luasTanamanAkhirBulanLaporan?: string;
+    produksiKuintal: {
+        dipanenHabis?: number;
+        belumHabis?: number;
+    }
+    rataRataHargaJual?: string;
+    keterangan?: string;
+}
 
 const LuasKabPage = () => {
     const [date, setDate] = React.useState<Date>()
+
+    const data: Data[] = [
+        {
+            kecamatan: "Metro Kibang",
+            desa: "Metro",
+            hasilProduksi: "12000",
+            namaTanaman: "50000",
+            luasTanamanAkhirBulanLalu: "100000",
+            luasPanen: {
+                habisDibongkar: 23,
+                belumHabis: 345,
+            },
+            luasRusak: "100",
+            luasPenanamanBaru: "100",
+            luasTanamanAkhirBulanLaporan: "100",
+            produksiKuintal: {
+                dipanenHabis: 23,
+                belumHabis: 345,
+            },
+            rataRataHargaJual: "100",
+            keterangan: "100",
+        },
+    ];
 
     return (
         <div>
@@ -137,6 +194,255 @@ const LuasKabPage = () => {
                 </div>
             </div>
             {/* top */}
+
+            {/* table */}
+            <Table className='border border-slate-200 mt-4'>
+                <TableHeader className='bg-primary-600'>
+                    <TableRow >
+                        <TableHead colSpan={10} className='text-primary py-1 border border-slate-200 text-center'>
+                            Atap 2022
+                        </TableHead>
+                        <TableHead colSpan={9} className='text-primary py-1 border border-slate-200 text-center'>
+                            Asem 2023
+                        </TableHead>
+                    </TableRow>
+                    <TableRow >
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            No
+                        </TableHead>
+
+                        {/* Atap */}
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="text-center items-center">
+                                Komoditi
+                            </div>
+                        </TableHead>
+                        <TableHead colSpan={3} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Komposisi Luas Areal
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200 text-center">
+                            <div className="text-center items-center">
+                                Jumlah
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200 text-center">
+                            <div className="text-center items-center">
+                                Produksi (TON)
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Produktivitas Kg/Ha
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Jumlah Petani Perkebunan (KK)
+                            </div>
+                        </TableHead>
+
+                        {/* Asem */}
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="text-center items-center">
+                                Komoditi
+                            </div>
+                        </TableHead>
+                        <TableHead colSpan={3} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Komposisi Luas Areal
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200 text-center">
+                            <div className="text-center items-center">
+                                Jumlah
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200 text-center">
+                            <div className="text-center items-center">
+                                Produksi (TON)
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Produktivitas Kg/Ha
+                            </div>
+                        </TableHead>
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            <div className="w-[150px] text-center items-center">
+                                Jumlah Petani Perkebunan (KK)
+                            </div>
+                        </TableHead>
+
+                        <TableHead rowSpan={2} className="text-primary py-1 border border-slate-200">
+                            Aksi
+                        </TableHead>
+                    </TableRow>
+                    <TableRow>
+                        {/* Atap */}
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TBM
+                        </TableHead>
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TM
+                        </TableHead>
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TR
+                        </TableHead>
+
+                        {/* Asem */}
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TBM
+                        </TableHead>
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TM
+                        </TableHead>
+                        <TableHead className="text-primary py-1 border border-slate-200 text-center">
+                            TR
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow >
+                        <TableCell className='border border-slate-200 text-center'>
+                            I
+                        </TableCell>
+                        <TableCell className='border border-slate-200 font-semibold'>
+                            Tan. Tahunan
+                        </TableCell>
+                    </TableRow>
+                    {data.map((item, index) => (
+                        <TableRow key={index}>
+                            {/* Atap */}
+
+                            <TableCell className='border border-slate-200 text-center'>
+                            </TableCell>
+                            <TableCell className='border border-slate-200'>
+                                {item.namaTanaman}
+                            </TableCell>
+                            <TableCell className='border border-slate-200'>
+                                {item.hasilProduksi}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasTanamanAkhirBulanLalu}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPanen.habisDibongkar}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPanen.belumHabis}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasRusak}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPenanamanBaru}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasTanamanAkhirBulanLaporan}
+                            </TableCell>
+
+                            {/* Asam */}
+                            <TableCell className='border border-slate-200 text-center'>
+                                {index + 1}
+                            </TableCell>
+                            <TableCell className='border border-slate-200'>
+                                {item.namaTanaman}
+                            </TableCell>
+                            <TableCell className='border border-slate-200'>
+                                {item.hasilProduksi}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasTanamanAkhirBulanLalu}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPanen.habisDibongkar}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPanen.belumHabis}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasRusak}
+                            </TableCell>
+                            <TableCell className='border border-slate-200 text-center'>
+                                {item.luasPenanamanBaru}
+                            </TableCell>
+
+                            <TableCell>
+                                <div className="flex items-center gap-4">
+                                    <Link className='' href="/perkebunan/luas-produksi-kabupaten/detail">
+                                        <EyeIcon />
+                                    </Link>
+                                    <Link className='' href="/perkebunan/luas-produksi-kabupaten/edit">
+                                        <EditIcon />
+                                    </Link>
+                                    <DeletePopup onDelete={() => { }} />
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                    <TableRow>
+                        <TableCell className='border border-slate-200'>
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            Jumlah
+                        </TableCell>
+
+                        {/* jumlah */}
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        {/* Jumlah */}
+
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                            234
+                        </TableCell>
+                        <TableCell className='border font-semibold border-slate-200 text-center'>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            {/* table */}
         </div>
     )
 }
