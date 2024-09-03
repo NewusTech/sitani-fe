365a7b7788d6099a2c73500b7ac721dde3ced0c8
hariando7 <rianaja2200022@gmail.com>
2024-09-03 10:21:30 +0700

    detail responsive

--- src/app/(superadmin)/ketahanan-pangan/produsen-dan-eceran/detail/page.tsx
+++ src/app/(superadmin)/ketahanan-pangan/produsen-dan-eceran/detail/page.tsx
@@ -10,8 +10,8 @@ interface LabelProps {
 
 const LabelDetail = (props: LabelProps) => {
   return (
-    <div className='flex gap-2 justify-between lg:justify-start'>
-      <div className="label text-black">{props.label || '-'} : </div>
+    <div className='flex gap-2 justify-between lg:justify-start lg:block lg:flex-none'>
+      <div className="label text-black">{props.label || '-'}</div>
       <div className="name text-black/70">{props.name || '-'}</div>
     </div>
   )
